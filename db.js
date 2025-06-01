7a66db11137efad8cca30b66f5fb9a1a882edab1
Nirvairjot kaur <166851120+Nirvairr@users.noreply.github.com>
2025-06-01 18:39:16 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Noticed");
+    await mongoose.connect("mongodb+srv://nirvairjot710:$$$SkyBlue0710$$$@cluster0.pjgkh0y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("Connected To MongoDB Successfully");
   } catch (error) {
     console.log(error);
