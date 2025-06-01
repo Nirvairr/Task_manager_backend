854fb9cfd2fd46e8ddf3c6fea76925f8b9e8b59d
Nirvairjot kaur <166851120+Nirvairr@users.noreply.github.com>
2025-06-01 18:52:08 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
+require('dotenv').config();
+
+
 const connectToMongo = async () => {
   try {
-    await mongoose.connect("mongodb+srv://nirvairjot710:%24%24%24SkyBlue0710%24%24%24@cluster0.pjgkh0y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-");
+    await mongoose.connect(process.env.Mongo_URI);
     console.log("Connected To MongoDB Successfully");
   } catch (error) {
     console.log(error);
